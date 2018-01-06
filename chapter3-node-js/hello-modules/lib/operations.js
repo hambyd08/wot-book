fd1dd187b5e4a71105012c9475bd004d025996b2
hambyd08 <hambyd08@gmail.com>
2018-01-06 16:36:41 -0500

    Test Commit

--- chapter3-node-js/hello-modules/lib/operations.js
+++ chapter3-node-js/hello-modules/lib/operations.js
@@ -1,17 +1,17 @@
 exports.add = function(a, b) {  //#A
   logOp(a, b, '+');
   return a + b;
-}
+};
 
 exports.sub = function(a, b) {
   logOp(a, b, '-');
   return a - b;
-}
+};
 
 exports.mul = function(a, b) {
   logOp(a, b, '*');
   return a * b;
-}
+};
 
 function logOp(a, b, op) {  //#B
   console.log('Computing ' + a + op + b);
